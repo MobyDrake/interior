3962d09020064a971c491f103b9f6598623bcf06
Milkky <Milkky598@yandex.ru>
2020-08-28 13:26:39 +0500

    Initial commit

---
+++ src/main/java/ru/mobydrake/interior/InteriorApplication.java
@@ -0,0 +1,13 @@
+package ru.mobydrake.interior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteriorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteriorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mobydrake/interior/InteriorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mobydrake.interior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteriorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
